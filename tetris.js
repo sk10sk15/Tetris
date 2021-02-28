0d7e101a31726ff97e2c5454c8221f43a1ce41fc
sk10sk15 <sksk2525oreimo@gmail.com>
2021-02-28 19:49:37 +0900

    add js file

--- tetris.js
+++ tetris.js
@@ -49,4 +49,20 @@ class Field {
     tileAt(x,y) {
         return this.tiles[y][x];
     }
+    draw(){
+        for(let y=0; y<21; y++){
+            for(let x=0; x<12; x++){
+                if(this.tileAt(x,y) === 0) continue;
+                new Block(x,y).draw();
+            }
+        }
+    }
+}
+
+function setup(){
+    createCanvas(400, 600);
+    background(64);
+}
+function draw(){
+
 }
